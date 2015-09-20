b68ac40bd52e86fd8a1a6b20dffc8dcd8c81687a
EC2 Default User <ec2-user@ip-172-31-19-63.us-west-2.compute.internal>
2015-09-20 10:42:41 +0000

    fix ddl

--- db/db.sh
+++ db/db.sh
@@ -1,6 +1,9 @@
 #!/bin/bash
+
+# make db
 mysql -u root -e "create database organic_kitchen default charset utf8"
 
+# make tables in db
 mysql -u root -D organic_kitchen < client.ddl
 mysql -u root -D organic_kitchen < product.ddl
 mysql -u root -D organic_kitchen < purchase_history.ddl
